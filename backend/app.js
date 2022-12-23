c0fa540462b1afd5b748e336a6a3123b532b891d
YANGSANGHEE <13614024ysh@gmail.com>
2022-12-23 14:49:56 +0900

    구축

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,5 @@
 const express = require("express"); // express
 const cors = require("cors");
-const fs = require('fs')
-const path = require('path')
 const router = require("./Router/ApiSetGET");
 
 
@@ -9,16 +7,18 @@ const app = express();
 const Port = process.env.Port || 4545;
 
 app.use(
-  cors()
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
-app.use(express.static(path.join(__dirname, '../dist')));
 
 app
   .get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist', "index.html"))
+    res.send(`server Start ${Port}`);
   })
   .listen(Port, err => {
     if (err) throw err;
